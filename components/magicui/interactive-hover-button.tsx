6ea4d245f8d733a081c23300a24cbeefc76ddd31
pratul03 <kk953504@gmail.com>
2025-02-12 00:02:24 +0530

    mn

--- components/magicui/interactive-hover-button.tsx
+++ components/magicui/interactive-hover-button.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 
 import React from "react";
 import { ArrowRight } from "lucide-react";
